02e5e02e708e3d72b39bb52b20efc055facb4d18
Büşra Sunar <54499947+BusraSunar@users.noreply.github.com>
2019-10-12 00:36:11 +0300

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,24 +1,19 @@
 package com.mycompany.app;
-import java.util.*;
-
-/**
- * Hello world!
- *
- */
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
 public class App
-{
-
+   {
      public static boolean search(ArrayList<Integer> array, int e) {
-         System.out.println("inside search");
-         if (array == null) return false;
-         for (int elt : array) {
-             if (elt == e) return true;
-         }         return false;
-     }
-
-
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+          System.out.println("inside search");
+          if (array == null) return false;
+          for (int elt : array) {
+               if (elt == e) return true;
+          }
+          return false;
+   }
