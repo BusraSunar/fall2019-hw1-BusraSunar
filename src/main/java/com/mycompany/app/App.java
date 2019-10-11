59fe37210f9a58c3e009f1dd4b802028c7f5fb74
Büşra Sunar <54499947+BusraSunar@users.noreply.github.com>
2019-10-12 00:40:11 +0300

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -55,3 +55,6 @@ public class App
       return Integer.parseInt(processBuilder.environment().get("PORT"));
       }     
    }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+   }
+}
